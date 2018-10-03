b548991b87c0c22d36ad37e0609d4f6a1fe66351
Dan Kalinin <daniil551@hotmail.com>
2018-10-03 13:23:27 +0300

    Try wrapper

--- AVFoundationExt/AVEVoiceManager.m
+++ AVFoundationExt/AVEVoiceManager.m
@@ -33,49 +33,45 @@
 - (instancetype)init {
     self = super.init;
     if (self) {
-        self.session = AVEAudioSession.shared;
-        [self.session.delegates addObject:self.delegates];
-        
-        [self.session.audioSession setCategory:AVAudioSessionCategoryPlayAndRecord mode:AVAudioSessionModeVoiceChat options:0 error:NULL];
-        [self.session.audioSession setPreferredIOBufferDuration:0.005 error:NULL];
-        [self.session.audioSession setPreferredSampleRate:44100.0 error:NULL];
-        
-        AudioComponentDescription description = {0};
-        description.componentType = kAudioUnitType_Output;
-        description.componentSubType = kAudioUnitSubType_VoiceProcessingIO;
-        description.componentManufacturer = kAudioUnitManufacturer_Apple;
-        
-        self.unit = [AVEAudioUnit.alloc initWithComponentDescription:description];
-        [self.unit.delegates addObject:self.delegates];
-        
-        self.unit.inputs[1].kAudioOutputUnitProperty_EnableIO = 1;
-        
-        AudioStreamBasicDescription inputFormat = self.unit.inputs[1].kAudioUnitProperty_StreamFormat;
-        self.unit.inputs[0].kAudioUnitProperty_StreamFormat = inputFormat;
-        
-        AudioStreamBasicDescription outputFormat = self.unit.outputs[0].kAudioUnitProperty_StreamFormat;
-        self.unit.outputs[1].kAudioUnitProperty_StreamFormat = outputFormat;
-        
-        self.unit.global.kAudioUnitProperty_MaximumFramesPerSlice = 4096;
-        
-        self.unit.inputs[0].kAudioUnitProperty_SetRenderCallback = self.unit.inputs[0].renderCallback;
-        
-        [self.session.audioSession setActive:YES error:NULL];
-        
-        [self.unit initialize];
-        [self.unit start];
-        
-//        NSLog(@"ie - %u", self.unit.inputs[1].kAudioOutputUnitProperty_EnableIO);
-//        NSLog(@"oe - %u", unit.outputs[0].kAudioUnitProperty_StreamFormat.mFormatID);
-        
-//        unit.inputs[1].kAudioUnitProperty_ElementName = @"xxx";
-//        NSLog(@"name - %@", unit.inputs[1].kAudioUnitProperty_ElementName);
-        
-//        NSLog(@"inputs - %u", unit.global.kAudioUnitProperty_MaximumFramesPerSlice);
-//        NSLog(@"outputs - %i", (int)unit.outputs.count);
-//        AVEAudioUnitInstantiation *instantiation = [unit instantiateWithOptions:kAudioComponentInstantiation_LoadOutOfProcess];
-//        instantiation.delegates.operationQueue = nil;
-//        [instantiation waitUntilFinished];
+        @try {
+            self.session = AVEAudioSession.shared;
+            [self.session.delegates addObject:self.delegates];
+            
+            [self.session.audioSession setCategory:AVAudioSessionCategoryPlayAndRecord mode:AVAudioSessionModeVoiceChat options:0 error:NULL];
+            [self.session.audioSession setPreferredIOBufferDuration:0.005 error:NULL];
+            [self.session.audioSession setPreferredSampleRate:44100.0 error:NULL];
+            
+            AudioComponentDescription description = {0};
+            description.componentType = kAudioUnitType_Output;
+            description.componentSubType = kAudioUnitSubType_VoiceProcessingIO;
+            description.componentManufacturer = kAudioUnitManufacturer_Apple;
+            
+            self.unit = [AVEAudioUnit.alloc initWithComponentDescription:description];
+            [self.unit.delegates addObject:self.delegates];
+            
+            self.unit.inputs[1].kAudioOutputUnitProperty_EnableIO = 1;
+            
+            AudioStreamBasicDescription inputFormat = self.unit.inputs[1].kAudioUnitProperty_StreamFormat;
+            self.unit.inputs[0].kAudioUnitProperty_StreamFormat = inputFormat;
+            
+            AudioStreamBasicDescription outputFormat = self.unit.outputs[0].kAudioUnitProperty_StreamFormat;
+            self.unit.outputs[1].kAudioUnitProperty_StreamFormat = outputFormat;
+            
+            self.unit.global.kAudioUnitProperty_MaximumFramesPerSlice = 4096;
+            
+            self.unit.inputs[0].kAudioUnitProperty_SetRenderCallback = self.unit.inputs[0].renderCallback;
+            
+            [self.session.audioSession setActive:YES error:NULL];
+            
+            [self.unit initialize];
+            [self.unit start];
+            
+//            NSLog(@"ie - %u", self.unit.inputs[1].kAudioOutputUnitProperty_EnableIO);
+        } @catch (NSException *exception) {
+            
+        } @finally {
+            
+        }
     }
     return self;
 }
